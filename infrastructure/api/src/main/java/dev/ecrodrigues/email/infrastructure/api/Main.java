f0c1e5aeb0d83341a53bcfb83209cb36f4f782e2
Emerson Rodrigues <ecr.developer23@gmail.com>
2024-01-08 21:24:00 +0100

    feat: [CHAP-101] Configuring the submodules of the base project

---
+++ infrastructure/api/src/main/java/dev/ecrodrigues/email/infrastructure/api/Main.java
@@ -0,0 +1,11 @@
+package dev.ecrodrigues.email.infrastructure.api;
+
+import dev.ecrodrigues.email.application.UseCase;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println(new UseCase().execute());
+  }
+}
